1a71ecb68e252070d825d8085d4223a36d86b4e8
Wagner Terry <goncalveswagner15@gmail.com>
2023-09-13 17:45:53 -0300

    chore: configurando AuthController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: `3s3)|&S7'{MH<N5I&|>'{P.>-uR9-5;Y`,
     }),
+    UserModule,
+    PrismaModule,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
