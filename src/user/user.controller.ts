1a3c26b91d13f279aa42ea2ebc40fd6118f5ef97
Wagner Terry <goncalveswagner15@gmail.com>
2023-09-13 15:42:33 -0300

    feat: Criando um decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,6 +15,7 @@ import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptor';
+import { ParamId } from 'src/decorators/param-id.decorator';
 
 @UseInterceptors(LogInterceptor)
 @Controller('users')
@@ -32,7 +33,7 @@ export class UserController {
   }
 
   @Get(':id')
-  async show(@Param('id', ParseIntPipe) id: number) {
+  async show(@ParamId() id: number) {
     return this.userService.show(id);
   }
 
